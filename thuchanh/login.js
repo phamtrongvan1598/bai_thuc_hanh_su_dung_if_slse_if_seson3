b58598d4a5455e1cb660e5aa7391737828d47481
phamtrongvan1598 <phamtrongvan9xbn@gmail.com>
2019-04-26 15:01:07 +0700

    login

---
+++ thuchanh/login.js
@@ -0,0 +1,14 @@
+let userName = prompt("ai đó",);
+if  (userName =="admin"
+){
+alert("thành công");}
+else if (userName == null){
+    alert("xin nhập lại");
+} else {alert(" bạn không thể đăng nhập");}
+let pass = prompt("mật mã",);
+if (pass == 'thit_cho'){
+    alert('thành công');
+}else if (pass == null){
+    alert('không đúng');
+}
+alert('xin hãy đăng nhập lại')
